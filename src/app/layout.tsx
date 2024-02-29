644d9e1a2ffc87bc6950cae6992e23ca476e6bcd
Haoyuan Zhang <haoyuaz9@uci.edu>
2024-02-29 23:10:52 +0800

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import NavBar from './Components/nav-bar.jsx'
+import FootBar from './Components/foot-bar.jsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div lang="en" id ="container">
+      <div className="flex flex-col min-h-screen bg-gray-200">
+        {/* header */}
+        <header>
+          <NavBar/>
+        </header>
+
+        {/* main body */}
+        <main>
+          <body className={inter.className}>{children}</body>
+        </main>
+
+        {/* footer */}
+        <FootBar/>
+      </div>
+    </div>
   )
 }
---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import MyDropdown from "../Components/drop-menu.jsx"
+
+export default function test() {
+    return(
+        <div>
+            <MyDropdown/>
+        </div>
+    )
+}
